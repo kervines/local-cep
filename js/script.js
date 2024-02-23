82c950495053add85a5fb5beea42c8f873972fdf
kervines <kervinzero@gmail.com>
2024-02-22 22:36:54 -0300

    refatoring - refatorado code de requisicao api

--- js/script.js
+++ js/script.js
@@ -3,14 +3,18 @@ const dataCep = document.querySelectorAll('[data-cep]');
 const form = document.querySelector('.search');
 const result = document.querySelector('.result');
 
-form.addEventListener('submit', (e) => {
+const findCep = async (cep) => {
+  const cepFetch = (await fetch(`http://viacep.com.br/ws/${cep}/json/`)).json();
+  const dataCep = await cepFetch;
+  return dataCep;
+  // const { bairro, cep, complemento, ddd, localidade, logradouro, uf } = dataCep;
+};
+
+const handleCEP = (e) => {
   e.preventDefault();
   if (inputCep.value) {
-    const apiCep = fetch(`http://viacep.com.br/ws/${inputCep.value}/json/`);
+    const apiCep = findCep(inputCep.value);
     apiCep
-      .then((resolve) => {
-        return resolve.json();
-      })
       .then((dados) => {
         dataCep.forEach((data) => {
           data.innerText = dados[data.id];
@@ -23,4 +27,6 @@ form.addEventListener('submit', (e) => {
         alert('CEP Inválido');
       });
   }
-});
+};
+
+form.addEventListener('submit', handleCEP);
